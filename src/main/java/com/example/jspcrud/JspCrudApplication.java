960a92769bad9c56bd5a6e402d8a69e9f0a9d3a6
jang-yujin <jyj9747@naver.com>
2023-01-05 15:59:13 +0900

    jspCRUD 공부

---
+++ src/main/java/com/example/jspcrud/JspCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.jspcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jspcrud/JspCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jspcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
